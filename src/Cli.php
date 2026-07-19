3f76542a85eb32536a4fb0944558de59bac3c960
Lev <alexfrolovyh@list.ru>
2026-07-19 13:24:14 +0300

    Update Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -9,5 +9,6 @@ function getName()
 {
   line('Welcome to the Brain Game!');
   $name = prompt('May I have your name?');
-  line("Hello, %s!", $name);
+  $greetings = line("Hello, %s!", $name);
+  return $greetings;
 }
