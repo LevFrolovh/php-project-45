80457572acf4514e4aaf05558cb8aad7a00f67f9
Lev <alexfrolovyh@list.ru>
2026-08-05 12:32:24 +0300

    Update Even.php

--- src/Even.php
+++ src/Even.php
@@ -25,8 +25,8 @@ function even()
     while ($round < 3) {
         $number = rand(0, 100);
         $rightAnswer = isEven($number);
-        $answer = line("Question: {$number}");
-        promt("Your answer: %s", $answer);
+        $answer = promt("Question: {$number}");
+        line("Your answer: %s", $answer);
         if ($answer === $rightAnswer) {
             line('Correct!');
             $round++;
