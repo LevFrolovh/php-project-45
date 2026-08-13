d46a41558bd02095eab503e1b5157f48e61321d9
Lev <alexfrolovyh@list.ru>
2026-08-13 15:00:32 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -1,3 +1,18 @@
 <?php
 
-namespace BrainGames\Engine
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function engine($rightAnswer)
+{
+    $answer = prompt('Your answer');
+    if ($answer === $rightAnswer) {
+        line('Correct!');
+    } else {
+        line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
+        line("Let's try again, {$name}!");
+        $win = false;
+    }
+}
