48a3b0b38e7ded14a61d4ee0c451e68f58751488
Lev <alexfrolovyh@list.ru>
2026-08-16 12:28:24 +0300

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -4,17 +4,31 @@ namespace BrainGames\Engine;
 
 use function cli\line;
 use function cli\prompt;
+use function BrainGames\Cli\greetings;
 
-function engine($win, $round, $rightAnswer)
+function engine($getQuestionAndAnswer, $gameDescription)
 {
-    $answer = prompt('Your answer');
-    if ($answer === $rightAnswer) {
-        line('Correct!');
-        return $round++;
-    } else {
-        line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
-        line("Let's try again, {$name}!");
-        $win = false;
-        return $win;
+    $name = greetings();
+    line($gameDescription);
+    
+    $round = 0;
+    $win = true;
+    while ($round < 3 && $win === true) {
+        
+        [$question, $rightAnswer] = $getQuestionAndAnswer;
+        line("Question: {$question}");
+        $answer = prompt('Your answer');
+        
+        if ($answer === $rightAnswer) {
+            line('Correct!');
+            $round++;
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
+            line("Let's try again, {$name}!");
+            $win = false;
+        }
+    }
+    if ($win) {
+        line("Congratulations, {$name}!");
     }
 }
