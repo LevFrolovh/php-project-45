b08928a85af0ba44055456a0ba780f420e1eb5b0
Lev <alexfrolovyh@list.ru>
2026-08-17 11:10:33 +0300

    Create Gcd.php

---
+++ src/Games/Gcd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Gcd;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Cli\greetings;
+use function BrainGames\Engine\engine;
+
+function gcd()
+{
+  
+}
