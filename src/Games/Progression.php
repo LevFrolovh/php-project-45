f8556c531672cfae4543a2ed616e1b590088b010
Lev <alexfrolovyh@list.ru>
2026-08-21 17:06:15 +0300

    Update Progression.php

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -6,7 +6,34 @@ use function cli\line;
 use function cli\prompt;
 use function BrainGames\Engine\engine;
 
+function getProgression()
+{
+    $start = rand(0, 100);
+    $progression[] = $start;
+    $step = rand(0, 15);
+    $lenght = rand(5, 10);
+    for ($i = 1; $i < $lenght; $i++) {
+        $progression[$i] = $start + $i * $step;
+    }
+    return $progression;
+}
 
+function getHiddenNum($progression)
+{
+    $lenght = count($progression);
+    $hiddenNumIndex = rand(0, $lenght - 1);
+    $hiddenNum = $progression[$hiddenNumIndex];
+    return $hiddenNum;
+}
+
+function getQuestionAndAnswer()
+{
+    $progression = getProgression();
+    $rightAnswer = getHiddenNum($progression);
+    $strProgression = implode(" ", $progression);
+    $question = str_replace($rightAnswer, '..', $strProgression);
+    return [$question, $rightAnswer];
+}
 
 function progression()
 {
