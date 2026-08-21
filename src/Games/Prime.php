d0a5dc0baf5f7f1d8a6792e8fb6fa7c4d5902847
Lev <alexfrolovyh@list.ru>
2026-08-21 22:33:10 +0300

    Update Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,8 +8,11 @@ use function BrainGames\Engine\engine;
 
 function prime()
 {
+    echo "Hello lev!";
+    /*
     $gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
     engine(function () {
         return getQuestionAndAnswer();
     }, $gameDescription);
+    */
 }
