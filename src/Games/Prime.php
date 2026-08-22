f2277931eee7cc2fe17b58c06622dc16df179ab2
Lev <alexfrolovyh@list.ru>
2026-08-22 12:08:10 +0300

    Update Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -6,13 +6,29 @@ use function cli\line;
 use function cli\prompt;
 use function BrainGames\Engine\engine;
 
-function prime()
+function isPrime($number)
 {
-    echo "Hello lev!";
-    /*
-    $gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-    engine(function () {
-        return getQuestionAndAnswer();
-    }, $gameDescription);
-    */
+    if ($number <= 1) {
+        return 'no';
+    }
+    if ($number > 3) {
+        if ($number % 2 === 0 || $number % 3 === 0) {
+            return 'no';
+        } else {
+            for ($i = 5; $i * $i <= $number; $i += 2) {
+                if ($number % $i === 0) {
+                    return 'no';
+                }
+            }
+        }
+    }
+    return 'yes';
+}
+
+function getQuestionAndAnswer()
+{
+    $number = rand(-100, 100);
+    $question = $number;
+    $rightAnswer = isPrime($number);
+    return [$question, $rightAnswer];
 }
